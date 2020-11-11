71361fc952c5271903b73a35ae1d5e7ad8a5dccb
Lucchetta <68827971+Lucchetta@users.noreply.github.com>
2020-11-10 18:25:53 -0800

    Removed some unnecessary files of the default attributes of the Git Hub Files
    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        
+        This project is created by Francesca McCall and is open-source
+          <a href="https://github.com/Lucchetta/react-weather-app"
+        target="_blank"></a>
+
+          
+
+      </footer>
+    
+    
+    </div>
+  );
 }
 

4b6e3c7803215b8f532e2084da80eff77de777d1
Lucchetta <68827971+Lucchetta@users.noreply.github.com>
2020-12-02 19:12:58 -0800

    Updates to all folders

--- src/App.js
+++ src/App.js
@@ -13,10 +13,13 @@ export default function App() {
       <footer>
         
         This project is created by Francesca McCall and is{" "}
-          <a href="https://github.com/Lucchetta/react-weather-app"
-        target="_blank">
-          open-source on github
-        </a>
+      <a
+          href="https://github.com/Lucchetta/react-weather-app"
+          target="_blank"
+          rel="noopener norferrer"
+          >
+            open-source on github
+          </a>
 
       
       </footer>
